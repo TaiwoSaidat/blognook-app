4c58f039b9fbb2356d5bd785cd1db12ecb02a560
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2025-03-19 11:48:20 +0100

    set up/first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="p-12 m-20 font-extrabold text-6xl bg-amber-500 text-gray-500 ">
+        hello world
+      </div>
+    </>
+  );
+}
